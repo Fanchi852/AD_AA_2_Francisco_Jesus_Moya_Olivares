3498f8c2a0caa5f581c3461d95bdfa091f353dfc
Fanchi852 <fjmo852@gmail.com>
2021-04-27 14:52:35 +0200

    Initial commit

---
+++ AD-AA-2-Francisco-Jesus-Moya-Olivares/src/main/java/com/sanvalero/ADAA2/Francisco/Jesus/Moya/Olivares/AdAa2FranciscoJesusMoyaOlivaresApplication.java
@@ -0,0 +1,13 @@
+package com.sanvalero.ADAA2.Francisco.Jesus.Moya.Olivares;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdAa2FranciscoJesusMoyaOlivaresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdAa2FranciscoJesusMoyaOlivaresApplication.class, args);
+	}
+
+}
---
+++ AD-AA-2-Francisco-Jesus-Moya-Olivares/src/test/java/com/sanvalero/ADAA2/Francisco/Jesus/Moya/Olivares/AdAa2FranciscoJesusMoyaOlivaresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanvalero.ADAA2.Francisco.Jesus.Moya.Olivares;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdAa2FranciscoJesusMoyaOlivaresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
